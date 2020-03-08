badc8b5ab767d2ba1a5e8563ea73f21937c9be91
bobro <bobrow.bobro2015@yandex.ru>
2020-03-08 21:08:36 +0300

    feat: do all task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    var str = "";
+    for(var i = 0; i < expr.length; i+=2) {
+        if (i %10 == 0)
+            str += "|";
+        if((expr[i] + expr[i+1]) == "00")
+            continue;
+        else if((expr[i] + expr[i+1]) == "10")
+            str += ".";
+        else if((expr[i] + expr[i+1]) == "11")
+            str += "-";
+        else if((expr[i] + expr[i+1]) == "**") {
+            str += " ";
+            i += 8;
+        }
+    }
+    var arr = str.split("|");
+    arr.shift();
+    var res = "";
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] == " ") {
+            res += " "
+            continue;
+        }
+        else {
+            res += MORSE_TABLE[arr[i]];
+        }
+    }
+    return res;
 }
 
 module.exports = {
